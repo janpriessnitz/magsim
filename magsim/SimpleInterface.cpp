bc09cdedcff00ab206864eb1ca6954cd7ddd7852
Jan Priessnitz <honya121@gmail.com>
2024-06-05 14:42:55 +0200

    wip simple itnerface

--- magsim/SimpleInterface.cpp
+++ magsim/SimpleInterface.cpp
@@ -16,6 +16,8 @@ public:
   SimpleInterfaceGenerator(const Config & config) : LatticeGenerator(config) {
     pos_list_ = config.Get("positions");
     anis_ = config.Get("anisotropy");
+    J_ = config.Get("exchange");
+    J_interface_ = config.Get("exchange_interface");
   }
 
   std::tuple<std::vector<vec3d>, std::vector<vec3d>> GeneratePositions() const {
@@ -42,6 +44,50 @@ public:
     return {positions, spins};
   }
 
+  std::vector<std::vector<std::tuple<size_t, real>>> GenerateExchange(
+  const PointLookup & point_lookup) const
+  {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::vector<vec3d> int_vecs = {
+      std::get<0>(cell_),
+      -1*std::get<0>(cell_),
+      std::get<1>(cell_),
+      -1*std::get<1>(cell_),
+      std::get<2>(cell_),
+      -1*std::get<2>(cell_),
+    };
+
+    std::vector<std::vector<std::tuple<size_t, real>>> exch_list;
+    exch_list.resize(point_lookup.points_.size());
+
+    #pragma omp parallel for
+    for (size_t ind = 0; ind < point_lookup.points_.size(); ++ind) {
+      // std::vector<std::tuple<size_t, real>> one_exch;
+      std::map<size_t, real> one_exch_map;
+      vec3d pos = point_lookup.points_[ind];
+      for (const auto & [int_vec, int_energy] : ints) {
+        auto sym_vecs = ApplySymmetry(int_vec, syms);
+        for (const auto & sym_vec : sym_vecs) {
+          vec3d partner_pos = pos + sym_vec;
+          auto [partner_ind, phase] = GetPoint(point_lookup, partner_pos);
+          if (partner_ind) {
+            one_exch_map[*partner_ind] = int_energy*phase;
+          }
+        }
+      }
+
+      std::vector<std::tuple<size_t, real>> one_exch;
+      one_exch.insert(one_exch.end(), one_exch_map.begin(), one_exch_map.end());
+      exch_list[ind] = one_exch;
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    global_timer.AddTime(stop - start, Timer::Section::GenExchange);
+
+    return exch_list;
+  }
+
   SpinLattice Generate() const {
     SpinLattice res;
 
@@ -53,7 +99,7 @@ public:
 
     PointLookup point_lookup(res.positions_);
     // res.exchange_ = GenerateExchange(point_lookup, exchange_ints_, syms_);
-    
+
     res.avg_spins_ = res.spins_;
     res.n_avgs_ = 1;
 
@@ -62,6 +108,8 @@ public:
 
   std::vector<vec3d> pos_list_;
   real anis_;
+  real J_;
+  real J_interface_;
 };
 
 int main(int argc, char **argv) {
